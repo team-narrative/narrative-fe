2ed86f8ce7b372c41628ca5b3da646763081e8a1
olliecomet <leyechi@gmail.com>
2019-12-06 12:14:50 -0800

    about developer cards comp test

--- src/components/about-us/__tests__/AboutDeveloper.test.js
+++ src/components/about-us/__tests__/AboutDeveloper.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import AboutDeveloperCard from '../AboutDeveloperCard';
 
-describe('AboutDeveloper component', () => {
+describe('AboutDeveloperCard component', () => {
   it('renders an about developer card', () => {
     const wrapper = shallow(<AboutDeveloperCard developerName="developer" developerImage="developer.jpg" developerBio="I am a developer"/>);
     expect(wrapper).toMatchSnapshot();
