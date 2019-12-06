793f51e9cff5d93de948e5fe06acd7f9e77899bb
olliecomet <leyechi@gmail.com>
2019-12-06 12:06:51 -0800

    about developer card test

---
+++ src/components/about-us/__tests__/AboutDeveloper.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import AboutDeveloperCard from '../AboutDeveloperCard';
+
+describe('AboutDeveloper component', () => {
+  it('renders an about developer card', () => {
+    const wrapper = shallow(<AboutDeveloperCard developerName="developer" developerImage="developer.jpg" developerBio="I am a developer"/>);
+    expect(wrapper).toMatchSnapshot();
+  });
+});
