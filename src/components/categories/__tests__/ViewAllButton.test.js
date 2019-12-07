ee5d1c7e5e40a4563259c7682a9b29e9c3613812
olliecomet <leyechi@gmail.com>
2019-12-06 16:57:18 -0800

    current story comp test, fixed other comp tests

--- src/components/categories/__tests__/ViewAllButton.test.js
+++ src/components/categories/__tests__/ViewAllButton.test.js
@@ -4,7 +4,7 @@ import ViewAllButton from '../ViewAllButton';
 
 describe('ViewAllButton component', () => {
   it('renders view all button', () => {
-    const wrapper = shallow(<ViewAllButton />);
+    const wrapper = shallow(<ViewAllButton handleViewClick={() => true}/>);
     expect(wrapper).toMatchSnapshot();
   });
 });
