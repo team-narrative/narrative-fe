639b91f70f066b0b5ca48d6b6ff32c816c8b72f2
antonella <antonella.gr27@gmail.com>
2019-12-06 17:07:12 -0800

    toggle between character name and edit view

--- src/components/character/Character.js
+++ src/components/character/Character.js
@@ -1,10 +1,13 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import ReactQuill from 'react-quill';
+import styles from './Character.css';
+import arrow from '../../assets/arrow.png';
 
 const Character = ({ handleSubmit, currentName, currentDescription }) => {
   const [name, setName] = useState(currentName);
   const [description, setDescription] = useState(currentDescription);
+  const [hidden, setHidden] = useState(true);
 
   const formats = [
     'header',
@@ -28,12 +31,19 @@ const Character = ({ handleSubmit, currentName, currentDescription }) => {
     handleSubmit(name, description);
   };
 
+  const toggle = () => {
+    setHidden(!hidden);
+  };
+
   return (
-    <form onSubmit={onSubmit}>
-      <input type="text" value={name} onChange={({ target }) => setName(target.value)} required />
-      <ReactQuill value={description} onChange={(value) => setDescription(value)} formats={formats} modules={modules} />
-      <button>SUBMIT</button>
-    </form>
+    <div className={styles.Character}>
+      <img src={arrow} onClick={toggle} />
+      {hidden ? <h2>{name}</h2> : <form onSubmit={onSubmit}>
+        <input type="text" value={name} onChange={({ target }) => setName(target.value)} required />
+        <ReactQuill value={description} onChange={(value) => setDescription(value)} formats={formats} modules={modules} />
+        <button>DONE</button>
+      </form>}
+    </div>
   );
 };
 
