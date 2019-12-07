ac1c93afd61c6d94b7cb559afe15c49be906ccbe
olliecomet <leyechi@gmail.com>
2019-12-06 17:45:30 -0800

    default view comp

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import UserCard from '../components/sidebar/UserCard';
-import StoryNavigation from '../components/sidebar/StoryNavigation';
+import UserCard from './UserCard';
+import StoryNavigation from './StoryNavigation';
 
-const Sidebar = ({ name, picture, handleLogout, handleAddStory, stories, handleSubmit }) => {
-  // Add actions, reducers, selectors, and hooks
-  
+const Sidebar = ({ name, picture, handleLogout, handleAddStory, stories, handleSubmit }) => {  
   return (
     <aside>
       <UserCard name={name} picture={picture} handleLogout={handleLogout} />
