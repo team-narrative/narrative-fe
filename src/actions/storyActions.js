5e13fc0508d5997166027e35ca096b67427c9923
Alex Spencer <alexander.j.spencer@gmail.com>
2019-12-09 10:55:07 -0800

    updates to request utility and creation of story reducer, actions, and services

--- src/actions/storyActions.js
+++ src/actions/storyActions.js
@@ -1,7 +1,17 @@
-import { postStory } from '../services/narrativeApi';
+import { postStory } from '../services/stories';
+
+// export const CREATE_STORY = 'CREATE_STORY';
+// export const createStory = (story, description) => ({
+//   type: CREATE_STORY,
+//   payload: postStory(story, description)
+// });
 
 export const CREATE_STORY = 'CREATE_STORY';
-export const createStory = (story, description) => ({
-  type: CREATE_STORY,
-  payload: postStory(story, description)
-});
+export const createStory = (userId, storyTitle, storySynopsis, storyGenre, storyTags) => dispatch => {
+  postStory(userId, storyTitle, storySynopsis, storyGenre, storyTags)
+    .then(newStory =>
+      dispatch({
+        type: CREATE_STORY,
+        payload: newStory
+      }));
+};
---
+++ src/reducers/index.js
@@ -0,0 +1,6 @@
+import { combineReducers } from 'redux';
+import story from './storyReducer';
+
+export default combineReducers({
+  story,
+});
