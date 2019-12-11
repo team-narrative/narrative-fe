ded3189f275a7fd12a9236fde747ab1ef3652ec5
Alex Spencer <alexander.j.spencer@gmail.com>
2019-12-11 08:19:39 -0800

    some actions and reducers

--- src/actions/characterActions.js
+++ src/actions/characterActions.js
@@ -1,4 +1,4 @@
-import { postCharactersByStoryId } from '../services/characters';
+import { postCharactersByStoryId, getCharactersByStoryId } from '../services/characters';
 
 export const CREATE_CHARACTER = 'CREATE_CHARACTER';
 export const createCharacter = (characterStoryId, characterName, characterDescription, characterTags) => dispatch => {
@@ -7,5 +7,15 @@ export const createCharacter = (characterStoryId, characterName, characterDescri
       dispatch({
         type: CREATE_CHARACTER,
         payload: newCharacter
+      }));
+};
+
+export const FETCH_CHARACTERS_BY_STORY_ID = 'FETCH_CHARACTERS_BY_STORY_ID';
+export const fetchCharactersByStoryId = (characterStoryId) => dispatch => {
+  getCharactersByStoryId(characterStoryId)
+    .then(fetchedCharacter =>
+      dispatch({
+        type: FETCH_CHARACTERS_BY_STORY_ID,
+        payload: fetchedCharacter
       }));
 };
--- src/reducers/characterReducer.js
+++ src/reducers/characterReducer.js
@@ -1,13 +1,17 @@
-import { CREATE_CHARACTER } from '../actions/characterActions';
+import { CREATE_CHARACTER, FETCH_CHARACTERS_BY_STORY_ID } from '../actions/characterActions';
 
 const initialState = {
-  characters: []
+  characters: [],
+  currentCharacterStory: '',
+  currentStoryCharacters: []
 };
 
 export default function reducer(state = initialState, action) {
   switch(action.type) {
     case CREATE_CHARACTER:
-      return { ...state, characters: [...state.characters, action.payload] };
+      return { ...state, characters: [...state.characters, action.payload], currentCharacterStory: action.payload.characterStoryId };
+    case FETCH_CHARACTERS_BY_STORY_ID:
+      return { ...state, currentStoryCharacters: [...state.currentStoryCharacters, action.payload] };
     default:
       return state;
   }
